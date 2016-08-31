695a0056749074720672d4080714dab51fd2a50c
mbuzzas <mbuzzas3@gmail.com>
2016-08-31 12:51:50 -0700

    commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+function fizzbuzz() {
+	for (var i = 1; i <= 100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			$('body').append('<p>fizz buzz</p>');
+		}
+		else if (i % 3 === 0) {
+			$('body').append('<p>fizz</p>');
+		}
+		else if (i % 5 === 0) {
+			$('body').append('<p>buzz</p>');
+		}
+		else {
+			$('body').append('<p>' + i + '</p>');
+		}
+	}
+}
+
+fizzbuzz();
+	
+})
